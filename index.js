df32a527fc03dc4b54d712cb14626f02521d0c27
ASIK-0 <exe.ashiqui@gmail.com>
2025-12-22 10:05:38 +0600

    Set up MongoDB connection and create user endpoint

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const port = process.env.PORT || 5000;
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@aplication1.govvz0x.mongodb.net/?appName=aplication1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    // database & collection
+    const database = client.db('blood-Donation')
+    const userCollections = database.collection('users')
+    const requestsCollection = database.collection("requests");
+    const paymentCollection = database.collection("payments");
+
+    // create user 
+    app.post('/users', async (req, res) => {
+      const userInfo = req.body;
+
+      userInfo.role = "donor";
+      userInfo.createdAt = new Date();
+      const result = await userCollections.insertOne(userInfo);
+      res.send(result)
+    })
+
+
+
+
+
+ 
+
+
+    // find
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Blood Donation Server is Running!');
